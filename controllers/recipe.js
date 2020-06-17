4bf199cbfda95b1329e74ccc93f2519e04d9397a
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-06-17 17:09:53 -0400

    feat: recipes coming in from database

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -4,24 +4,42 @@ const Recipe = require('../models/recipes.js');
 module.exports = { index, addRecipe, delRecipe };
 
 function index(req, res, next) {
+  // console.log("req in recipes conroller >>>>", req)
   // // Make the query object to use with user.find based up
   // the user has submitted the search form or now
   let modelQuery = req.query.name ? { name: new RegExp(req.query.name, 'i') } : {};
-  // Default to sorting by name
+  // // Default to sorting by name
   let sortKey = req.query.sort || 'name';
   User.find(modelQuery)
     .sort(sortKey).exec(function (err, users) {
       if (err) return next(err);
+      Recipe.find({}, function(error, recipes) {
+        console.log('recipes from databse >>>> ', (recipes))
+        res.render('recipes', {
+          users,
+          recipes,
+          user: req.user,
+          name: req.query.name,
+          sortKey
+        });
+      })
       // Passing search values, name & sortKey, for use in the EJS
-      res.render('recipes', {
-        users,
-        user: req.user,
-        name: req.query.name,
-        sortKey
-      });
-    });
+    //   res.render('recipes', {
+    //     users,
+    //     user: req.user,
+    //     name: req.query.name,
+    //     sortKey
+    //   });
+    // });
+  // Recipe.find({}, function (error, recipes){
+  //   if (error) console.log('ERROR when retireving recipes >>> ', error)
+  //   console.log('recipes from databse >>>> ', typeof(recipes))
+  //   res.render('recipes', {
+  //     recipes,
+  //     recipeArr: recipes[0]
+  //   })
+  })
 }
-
 
 function addRecipe(req, res, next) {
   // console.log('req body ', req.body)
